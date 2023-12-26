8834404acf5039e7c4b798c5e07de00425d22c37
Lucas Scommegna <scommegnal@hotmail.com>
2023-12-26 19:43:17 -0300

    O(n^2) exercise

---
+++ Python Data Structures and Algorithms/Big O/O-of-n-squared.py
@@ -0,0 +1,11 @@
+# O(n^2) case because it executes the code n * n times 
+def print_items(n):
+    for i in range(n):
+        for j in range(n):
+            print(i,j)
+  
+        
+      
+        
+
+print_items(10)
