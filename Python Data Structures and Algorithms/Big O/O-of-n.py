48d0c8e36cc428a465c13ecf7633e953ecc12776
Lucas Scommegna <scommegnal@hotmail.com>
2023-12-26 19:38:05 -0300

    big O first exercise

---
+++ Python Data Structures and Algorithms/Big O/O-of-n.py
@@ -0,0 +1,10 @@
+# Its O(n) case because it executes, in worst case, "n" times.
+#This particular case will always execute the code "n" times
+def print_items(n):
+    for i in range(n):
+        print(i)
+        
+      
+        
+
+print_items(10)
