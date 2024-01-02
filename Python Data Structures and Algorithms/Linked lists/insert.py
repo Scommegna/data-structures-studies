75a14360e566231ba6dc780a7211d5e6a0395894
Lucas Scommegna <scommegnal@hotmail.com>
2024-01-02 11:05:18 -0300

    feat: insert method for LL

---
+++ Python Data Structures and Algorithms/Linked lists/insert.py
@@ -0,0 +1,56 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+    
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+    
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
+    
+    #Inserts new node to LL
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        
+        if index == 0:
+            return self.prepend(value)
+        
+        if index == self.length:
+            return self.append(value)
+        
+        temp = self.get(index - 1)
+          
+        new_node = Node(value)
+          
+        new_node.next = temp.next
+        temp.next = new_node
+          
+        self.length += 1
+          
+        return True
