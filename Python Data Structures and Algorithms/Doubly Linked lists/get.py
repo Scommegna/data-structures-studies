b2cbfec200a374d40686ba60f51de50be4ef6fdb
Lucas Scommegna <scommegnal@hotmail.com>
2024-01-08 18:03:58 -0300

    feat: get method for DLL

---
+++ Python Data Structures and Algorithms/Doubly Linked lists/get.py
@@ -0,0 +1,30 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+        
+
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+     
+    # Gets node in given index and returns it   
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+            
+        temp = self.head
+        
+        if index < self.length / 2:
+            for _ in range(index):
+                temp = temp.next
+        else:
+            temp = self.tail
+            for _ in range(self.length - 1, index, -1):
+                temp = temp.prev
+                
+        return temp
