10a9c8f995539592869c0ef1e8841aa69a232731
Lucas Scommegna <scommegnal@hotmail.com>
2024-01-12 15:47:34 -0300

    fix: stack push method refactor

--- Python Data Structures and Algorithms/Stacks/push.py
+++ Python Data Structures and Algorithms/Stacks/push.py
@@ -12,6 +12,11 @@ class Stack:
         
     def push(self, value):
         new_node = Node(value)
-        new_node.next = self.top
-        self.top = new_node
+        
+        if self.height == 0:
+            self.top = new_node
+        else:
+            new_node.next = self.top
+            self.top = new_node
+            
         self.height += 1
