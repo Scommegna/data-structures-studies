96613d7fa173ec27e361955cd7fbd28f1c13fb71
Lucas Scommegna <scommegnal@hotmail.com>
2024-01-26 15:39:10 -0300

    feat: remove vertex method for graphs

---
+++ Python Data Structures and Algorithms/Graphs/remove_vertex.py
@@ -0,0 +1,13 @@
+class Graph:
+    def __init__(self):
+        self.adj_list = {}
+        
+    def remove_vertex(self, vertex):
+        if vertex in self.adj_list.keys():
+            for other_vertex in self.adj_list[vertex]:
+                self.adj_list[other_vertex].remove(vertex)
+            del self.adj_list[vertex]
+            
+            return True
+            
+        return False
