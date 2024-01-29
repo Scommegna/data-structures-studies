a968a5378429478759c244b4d8604fd9f8e5745d
Lucas Scommegna <scommegnal@hotmail.com>
2024-01-29 14:12:01 -0300

    feat: heap constructor and helper methods

---
+++ Python Data Structures and Algorithms/Heap/helper_methods.py
@@ -0,0 +1,16 @@
+class MaxHeap:
+    def __init__(self):
+        self.heap = []
+        
+    def _left_child(self, index):
+        return 2 * index + 1
+    
+    def _right_child(self, index): 
+        return 2 * index + 2
+    
+    def parent(self, index):
+        return (index - 1) // 2
+    
+    def swap(self, index1, index2):
+        self.heap[index1], self.heap[index2] = self.heap[index2], self.heap[index1]
+    
