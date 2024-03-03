71253f4ea8f2708dce714e869b4f58e80df01472
Lucas Scommegna <scommegnal@hotmail.com>
2024-03-03 11:00:45 -0300

    feat: swap first and last values exercise DLL

---
+++ Python Data Structures and Algorithms/Doubly Linked lists/Interview questions/swap_first_and_last.py
@@ -0,0 +1,46 @@
+"""
+Swap the values of the first and last node
+
+Method name:
+swap_first_last
+
+Note that the pointers to the nodes themselves are not swapped - only their values are exchanged.
+"""
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+        
+
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def swap_first_last(self):
+        if self.length > 0:
+            temp = self.head.value
+            self.head.value = self.tail.value
+            self.tail.value = temp
