81b264a6401c80eb64502ee8fb57eca314c1b99e
Lucas Scommegna <scommegnal@hotmail.com>
2024-03-08 12:23:50 -0300

    feat: pop for stack exercise

---
+++ Python Data Structures and Algorithms/Stacks/Interview questions/pop_for_stack.py
@@ -0,0 +1,33 @@
+"""
+Add a method to pop a value from the Stack implementation that we began in the last two Coding Exercises.
+"""
+
+class Stack:
+    def __init__(self):
+        self.stack_list = []
+
+    def print_stack(self):
+        for i in range(len(self.stack_list)-1, -1, -1):
+            print(self.stack_list[i])
+
+    def is_empty(self):
+        return len(self.stack_list) == 0
+
+    def peek(self):
+        if self.is_empty():
+            return None
+        else:
+            return self.stack_list[-1]
+
+    def size(self):
+        return len(self.stack_list)
+
+    def push(self, value):
+        self.stack_list.append(value)
+
+    def pop(self):
+        if self.is_empty():
+            return None
+        else:
+            return self.stack_list.pop();
+ 
