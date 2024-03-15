f4e1bf7aeca9995b65986e3051178f195d2ba327
Lucas Scommegna <scommegnal@hotmail.com>
2024-03-15 08:09:10 -0300

    feat: dequeue with stack interview question

---
+++ Python Data Structures and Algorithms/Queue/Interview questions/dequeue_with_stack.py
@@ -0,0 +1,28 @@
+"""
+You have been tasked with implementing a queue data structure using two stacks in Python, and you need to write the dequeue method.
+
+The dequeue method should remove and return the first element in the queue.
+"""
+
+class MyQueue:
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+        
+    def enqueue(self, value):
+        while len(self.stack1) > 0:
+            self.stack2.append(self.stack1.pop())
+        self.stack1.append(value)
+        while len(self.stack2) > 0:
+            self.stack1.append(self.stack2.pop())
+
+    def dequeue(self):
+        if len(self.stack1) > 0:
+            return self.stack1.pop()
+
+    def peek(self):
+        return self.stack1[-1]
+
+    def is_empty(self):
+        return len(self.stack1) == 0
+        
