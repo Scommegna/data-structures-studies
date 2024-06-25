4edea274d2676b28733524d8730ff25e5192c962
Lucas Scommegna <scommegnal@hotmail.com>
2024-06-25 07:15:21 -0300

    feat: max product exercise

---
+++ The Complete Data Structures and Algorithms Course in Python/Lists/Exercises/max_product_of_two_ints.py
@@ -0,0 +1,21 @@
+"""
+Max Product of Two Integers
+Find the maximum product of two integers in an array where all elements are positive.
+
+Example
+
+arr = [1, 7, 3, 4, 9, 5] 
+max_product(arr) # Output: 63 (9*7)
+"""
+
+def max_product(arr):
+    max_val = max(arr)
+    max_prod = 0
+    
+    
+    for num in arr:
+        if num != max_val and num * max_val > max_prod:
+            max_prod = num * max_val
+            
+    return max_prod
+
