2016eaca7fc479d880f6474e2af5098a7e898e7a
Lucas Scommegna <scommegnal@hotmail.com>
2025-03-29 15:17:20 -0300

    ll

--- The Complete Data Structures and Algorithms Course in Python/LL/ll_methods.py
+++ The Complete Data Structures and Algorithms Course in Python/LL/ll_methods.py
@@ -49,6 +49,7 @@ class LinkedList:
             
             while pos != position - 1:
                 aux_pointer = aux_pointer.next
+                pos += 1
                 
             new_node.next = aux_pointer.next
             aux_pointer.next = new_node
@@ -74,6 +75,84 @@ class LinkedList:
             current = current.next
             
         return False
+    
+    def get(self, index):
+        if index >= self.length or index < 0:
+            return None
+        else:
+            pos = 0
+            aux_node = self.head
+            
+            while pos != index:
+                aux_node = aux_node.next
+                pos += 1
+            
+            return aux_node
+        
+    def set(self, value, index):
+        temp = self.get(index)
+        
+        if temp:
+            temp.value = value
+            
+            return True
+
+        return False
+    
+    def pop_first(self):
+        if self.length > 0:
+            temp = self.head
+            self.head = self.head.next
+            temp.next = None
+            self.length -= 1
+            
+            if self.head == None:
+                self.tail = None
+            
+            return temp
+        else:
+            return None
+        
+    def pop(self):
+        if self.length == 0:
+            return None
+        else:
+            popped_node = self.tail
+            
+            if self.length == 1:
+                self.head = None
+                self.tail = None
+            else:
+                aux_node = self.get(self.length - 2)
+                aux_node.next = None
+                self.tail = aux_node
+            
+            self.length -= 1
+            
+            return popped_node
+        
+    def remove(self, index):
+        if index >= self.length or index < 0:
+            return None
+        elif index == 0:
+            return self.pop_first()
+        elif index == self.length - 1:
+            return self.pop()
+        else:
+            aux_node = self.get(index - 1)
+            
+            removed_node = aux_node.next
+            aux_node.next = removed_node.next
+            removed_node.next = None
+            
+            self.length -= 1
+            
+            return removed_node
+        
+    def delete(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
         
     def __str__(self):
         list_str = ""
